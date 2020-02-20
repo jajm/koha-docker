ac123ae79dc0201bd73cbf9ecd437f03af87f2ec
Julian Maurice <julian.maurice@biblibre.com>
2020-02-20 09:18:26 +0100

    Add master-slim

--- build.sh
+++ build.sh
@@ -7,4 +7,9 @@ docker build --tag julianmaurice/koha:19.05.07 --tag julianmaurice/koha:19.05 19
 docker build --tag julianmaurice/koha:19.11.02 --tag julianmaurice/koha:19.11 --tag julianmaurice/koha:latest 19.11
 docker build --tag julianmaurice/koha:master master
 
-# Remove dangling images by running `docker rmi $(docker images -f 'dangling=true' -q)`
+# Tag the intermediate image so it doesn't get removed by `docker image prune`
+docker build --target builder --tag koha:master-slim-builder master-slim
+docker build --tag julianmaurice/koha:master-slim master-slim
+
+# List dangling images with `docker images -f dangling=true`
+# Remove them with `docker image prune`
