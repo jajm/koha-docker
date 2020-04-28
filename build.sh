3d080634d3de46a1a79e3b57a2e3a715cfb5f2ae
Julian Maurice <julian.maurice@biblibre.com>
2020-04-28 11:53:58 +0200

    Update files for new minor releases

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-docker build --tag julianmaurice/koha:18.11.15 --tag julianmaurice/koha:18.11 18.11
-docker build --tag julianmaurice/koha:19.05.09 --tag julianmaurice/koha:19.05 19.05
-docker build --tag julianmaurice/koha:19.11.04 --tag julianmaurice/koha:19.11 --tag julianmaurice/koha:latest 19.11
+docker build --tag julianmaurice/koha:18.11.16 --tag julianmaurice/koha:18.11 18.11
+docker build --tag julianmaurice/koha:19.05.10 --tag julianmaurice/koha:19.05 19.05
+docker build --tag julianmaurice/koha:19.11.05 --tag julianmaurice/koha:19.11 --tag julianmaurice/koha:latest 19.11
 docker build --tag julianmaurice/koha:master master
 
 # Tag the intermediate image so it doesn't get removed by `docker image prune`
