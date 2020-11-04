017568fb4238bec2db140ea985bedccd2ee5325d
Julian Maurice <julian.maurice@biblibre.com>
2020-11-04 17:16:14 +0100

    Add master-mojo

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ docker build --tag julianmaurice/koha:19.05.16 --tag julianmaurice/koha:19.05 19
 docker build --tag julianmaurice/koha:19.11.11 --tag julianmaurice/koha:19.11 19.11
 docker build --tag julianmaurice/koha:20.05.05 --tag julianmaurice/koha:20.05 --tag julianmaurice/koha:latest 20.05
 docker build --tag julianmaurice/koha:master master
+docker build --tag julianmaurice/koha:master-mojo master-mojo
 
 # Tag the intermediate image so it doesn't get removed by `docker image prune`
 docker build --target builder --tag koha:master-slim-builder master-slim
