015e774e022212f971595317f0c16b564ab9228c
Julian Maurice <julian.maurice@biblibre.com>
2020-11-30 16:14:24 +0100

    Use mojolicious apps when possible (20.11, master, master-slim)

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ docker build --tag julianmaurice/koha:19.11.12 --tag julianmaurice/koha:19.11 19
 docker build --tag julianmaurice/koha:20.05.06 --tag julianmaurice/koha:20.05 20.05
 docker build --tag julianmaurice/koha:20.11.00 --tag julianmaurice/koha:20.11 --tag julianmaurice/koha:latest 20.11
 docker build --tag julianmaurice/koha:master master
-docker build --tag julianmaurice/koha:master-mojo master-mojo
 
 # Tag the intermediate image so it doesn't get removed by `docker image prune`
 docker build --target builder --tag koha:master-slim-builder master-slim
