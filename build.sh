02315e16a74c2fc42f17dd6bac7d4e497a8ed8a2
Julian Maurice <julian.maurice@biblibre.com>
2021-03-24 13:25:42 +0100

    Extract minor version from dockerfile

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 
 set -ex
 
-docker build --tag julianmaurice/koha:19.11.15 --tag julianmaurice/koha:19.11 19.11
-docker build --tag julianmaurice/koha:20.05.09 --tag julianmaurice/koha:20.05 20.05
-docker build --tag julianmaurice/koha:20.11.03 --tag julianmaurice/koha:20.11 --tag julianmaurice/koha:latest 20.11
+find_version() {
+    grep --max-count=1 --only-matching "$1\.[0-9][0-9]" "$1/Dockerfile"
+}
+
+for v in 19.11 20.05 20.11; do
+    docker build --tag julianmaurice/koha:$(find_version $v) --tag julianmaurice/koha:$v $v
+done
+docker tag julianmaurice/koha:20.11 julianmaurice/koha:latest
+
 docker build --tag julianmaurice/koha:master master
 
 # Tag the intermediate image so it doesn't get removed by `docker image prune`
