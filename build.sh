8935d6a266e05be15596e3a2eb2f5da37f32bdf1
Julian Maurice <julian.maurice@biblibre.com>
2021-12-01 09:46:27 +0100

    Stop building image for 19.11

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ find_version() {
     grep --max-count=1 --only-matching "$1\.[0-9][0-9]" "$1/Dockerfile"
 }
 
-for v in 19.11 20.05 20.11 21.05 21.11; do
+for v in 20.05 20.11 21.05 21.11; do
     docker build --tag julianmaurice/koha:$(find_version $v) --tag julianmaurice/koha:$v $v
 done
 docker tag julianmaurice/koha:21.11 julianmaurice/koha:latest
